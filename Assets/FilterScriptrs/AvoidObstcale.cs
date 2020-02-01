e58f226607836d785cc19f7dd23990a180d773c3
SokolDrAI <sokolmurturi@gmail.com>
2020-02-01 16:10:47 +0000

    ObstacleAvoidance

---
+++ Assets/FilterScriptrs/AvoidObstcale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Obstacle")]
+public class AvoidObstcale : ContextFilter
+{
+
+
+    public override List<TransformAgent> Filter(FlockAgent agent, List<TransformAgent> original)
+    {
+        List<TransformAgent> filtered = new List<TransformAgent>();
+
+        foreach (TransformAgent item in original)
+        {
+            if (item.transform.tag.Equals("Obstacle"))
+            {
+                filtered.Add(item);
+            }
+        }
+        return filtered;
+    }
+}
