b22e6bace1457fc7646793be96f68bfdca53cde3
Nathan John <vethan@unrulyattractions.com>
2020-02-01 16:11:26 +0000

    Add Rewired

---
+++ Assets/MarsPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class MarsPlayer : MonoBehaviour
+{
+    private Player player;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3( player.GetAxis("Horizontal"),0, player.GetAxis("Vertical"));
+
+        transform.transform.position += (Vector3)direction * Time.deltaTime * 30;
+    }
+}
