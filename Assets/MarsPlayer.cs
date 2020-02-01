e2afc9446857dea240b2086f6e94e4178f368ead
Nathan John <vethan@unrulyattractions.com>
2020-02-01 18:01:33 +0000

    Lots of stuff

--- Assets/MarsPlayer.cs
+++ Assets/MarsPlayer.cs
@@ -6,18 +6,58 @@ using Rewired;
 public class MarsPlayer : MonoBehaviour
 {
     private Player player;
-
+    private Rigidbody _rigidbody;
+    private LineRenderer lineRenderer;
+    Tower currentTower;
     // Start is called before the first frame update
     void OnEnable()
     {
         player = ReInput.players.GetPlayer(0);
+        _rigidbody = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Tower otherTower = other.GetComponent<Tower>();
+        if (otherTower !=null)
+        {
+            currentTower = otherTower;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        Tower otherTower = other.GetComponent<Tower>();
+        if (otherTower == currentTower)
+        {
+            currentTower = null;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 direction = new Vector3( player.GetAxis("Horizontal"),0, player.GetAxis("Vertical"));
+        _rigidbody.velocity = direction * 20;
 
-        transform.transform.position += (Vector3)direction * Time.deltaTime * 30;
+        if(player.GetButton("Interact") && currentTower != null)
+        {
+            currentTower.Repair(Time.fixedDeltaTime * 0.2f);
+        }
+       
+    }
+
+    private void Update()
+    {
+        lineRenderer.enabled = currentTower != null && currentTower.repairValue < 1;
+        if(lineRenderer.enabled)
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, currentTower.transform.position);
+        }
+        
     }
 }
---
+++ Assets/Tower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float repairValue { get; private set; }
+    ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+
+    public void Repair(float amount)
+    {
+        repairValue = Mathf.Clamp01(repairValue + amount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color color = Color.Lerp(Color.red, Color.green, repairValue);
+        GetComponent<Renderer>().material.color = color;
+        var emission = particles.emission;
+        emission.enabled = repairValue == 1;
+    }
+}
