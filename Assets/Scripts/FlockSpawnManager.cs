12ad60460c19bdf1ab324fb2856b2008a81dae18
SokolDrAI <sokolmurturi@gmail.com>
2020-02-02 14:19:07 +0000

    no message

--- Assets/Scripts/FlockSpawnManager.cs
+++ Assets/Scripts/FlockSpawnManager.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class FlockSpawnManager : MonoBehaviour
 {
-    public int numberOfFlocks;
-    public List<Flock> ourFlocks;
-    public int[] howManyShipsInEachFlock;
 
+    //Flock count
+    public int numberOfFlocks;
+    //Flocks
+    public List<GameObject> ourFlocks; 
+    //Count of ships in each flocks
+    public int howManyShipsInFlock;
+
+    //Upper and lower limit of ships
     public int maxShipCount;
     public int minShipCount;
 
+    //number of ships to spawn
     public int numberOfShipsToSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get a count of each of flocks in scene
+        numberOfFlocks = GameObject.FindGameObjectsWithTag("Flock").Length;
+
+        ourFlocks.AddRange(GameObject.FindGameObjectsWithTag("Flock"));
+        Debug.Log(ourFlocks);
     }
 
     // Update is called once per frame
@@ -23,4 +34,5 @@ public class FlockSpawnManager : MonoBehaviour
     {
         
     }
+
 }
